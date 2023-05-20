2c0ef15bee75560675f305e32407cccad7c82af1
Lau36 <laura.jaimes@correounivalle.edu.co>
2023-05-20 15:25:13 -0500

    Routes signIn and signUp

--- ProyectoIntegrador/src/App.js
+++ ProyectoIntegrador/src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./HomePage/homepage";
 import Principal from "./PrincipalPage/principalPage";
 import Temas from "./temasPage/temasPage";
+import SignIn from "./signIn/SingIn";
+import SignUp from "./signUp/SignUp";
 
 function App() {
   return (
     <div className="App">
-       <BrowserRouter>
-       <Routes>
-       <Route path="Home" element={<Home />} />
-       <Route path="Principal" element={<Principal />} />
-       <Route path="Temas" element={<Temas />} />
-       </Routes>
-       </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="Home" element={<Home />} />
+          <Route path="Principal" element={<Principal />} />
+          <Route path="Temas" element={<Temas />} />
+          <Route path="SignIn" element={<SignIn />} />
+          <Route path="SignUp" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
