30f8bd295b225e88efa567ba5b7b1af21c836fec
Lau36 <laura.jaimes@correounivalle.edu.co>
2023-05-20 17:17:10 -0500

    Styles and html

---
+++ ProyectoIntegrador/src/signUp/SignUp.js
@@ -0,0 +1,70 @@
+import "./StyleSignUp.css";
+import backgroundImage from "./Images/temploPoseidon.jpg";
+
+const SignIn = () => {
+  return (
+    <div
+      className="contenedorPrincipal"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        width: "100%",
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="formContainerSignUp">
+        <div className="form-group">
+          <label>Nombre: </label>
+          <input
+            type="text"
+            className="form-control"
+            name="nombre"
+            placeholder=" "
+            // onChange={this.handleChange}
+          />
+          <br />
+          <label>Apellido: </label>
+          <input
+            type="text"
+            className="form-control"
+            name="apellido"
+            placeholder=" "
+            // onChange={this.handleChange}
+          />
+          <br />
+          <label>Correo: </label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            placeholder=" "
+            // onChange={this.handleChange}
+          />
+          <br />
+          <label>Username: </label>
+          <input
+            type="text"
+            className="form-control"
+            name="username"
+            placeholder=" "
+            // onChange={this.handleChange}
+          />
+          <br />
+          <label>Contraseña: </label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            placeholder="********"
+            // onChange={this.handleChange}
+          />
+          <br />
+          <button type="submit">Registarme</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
