4aace8274acfae8a06f938dccf5db2ba3fad7e8f
DianaCadenaMoreno <diana.marcela.cadena@correounivalle.edu.co>
2023-05-20 22:31:23 -0500

    Seccion de Arte

--- ProyectoIntegrador/src/App.js
+++ ProyectoIntegrador/src/App.js
@@ -5,9 +5,9 @@ import Principal from "./PrincipalPage/principalPage";
 import Temas from "./temasPage/temasPage";
 import SignIn from "./signIn/SingIn";
 import SignUp from "./signUp/SignUp";
-import Mitologia from "./Costumbres/Mitologia";
-import Gastronomia from "./Costumbres/Gastronomia";
-import Deporte from "./Costumbres/Deporte";
+import Arte from "./Arte/Arte";
+import Literatura from "./Arte/Literatura";
+import Vestimenta from "./Arte/Vestimenta";
 
 function App() {
   return (
@@ -19,9 +19,9 @@ function App() {
           <Route path="Temas" element={<Temas />} />
           <Route path="SignIn" element={<SignIn />} />
           <Route path="SignUp" element={<SignUp />} />
-          <Route path="Mitologia" element={<Mitologia/>} />
-          <Route path="Gastronomia" element={<Gastronomia/>} />
-          <Route path="Deporte" element={<Deporte/>} />
+          <Route path="Arte" element={<Arte/>} />
+          <Route path="Literatura" element={<Literatura/>} />
+          <Route path="Vestimenta" element={<Vestimenta/>} />
         </Routes>
       </BrowserRouter>
     </div>
