87766efba97abeb59842c78fd558aa742f7cdde6
DianaCadenaMoreno <diana.marcela.cadena@correounivalle.edu.co>
2023-05-20 21:41:14 -0500

    Seccion de costumbres

--- ProyectoIntegrador/src/App.js
+++ ProyectoIntegrador/src/App.js
@@ -5,6 +5,9 @@ import Principal from "./PrincipalPage/principalPage";
 import Temas from "./temasPage/temasPage";
 import SignIn from "./signIn/SingIn";
 import SignUp from "./signUp/SignUp";
+import Mitologia from "./Costumbres/Mitologia";
+import Gastronomia from "./Costumbres/Gastronomia";
+import Deporte from "./Costumbres/Deporte";
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
           <Route path="Temas" element={<Temas />} />
           <Route path="SignIn" element={<SignIn />} />
           <Route path="SignUp" element={<SignUp />} />
+          <Route path="Mitologia" element={<Mitologia/>} />
+          <Route path="Gastronomia" element={<Gastronomia/>} />
+          <Route path="Deporte" element={<Deporte/>} />
         </Routes>
       </BrowserRouter>
     </div>
