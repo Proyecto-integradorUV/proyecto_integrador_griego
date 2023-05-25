3a9ba8e269d490ff86f629157f5905eb9f848f9a
JesusAdrianP <95390821+JesusAdrianP@users.noreply.github.com>
2023-05-25 13:08:17 -0500

    Update urls.py

--- BackendPI/users/urls.py
+++ BackendPI/users/urls.py
@@ -3,7 +3,7 @@ from .views import UserList, LoginView, LogoutView, CreateUserView
 
 
 urlpatterns = [
-    path('singup/', CreateUserView.as_view(), name='create_user'),
+    path('signup/', CreateUserView.as_view(), name='create_user'),
     path('list/', UserList.as_view(), name = 'user-list'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
