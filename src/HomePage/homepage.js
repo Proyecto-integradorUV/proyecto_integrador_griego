1e3a1e0fda225173e149c2cf7e6c8ca30a0488e9
Lau36 <laura.jaimes@correounivalle.edu.co>
2023-06-19 17:22:26 -0500

    lo de cerrar la page

--- src/HomePage/homepage.js
+++ src/HomePage/homepage.js
@@ -1,12 +1,22 @@
 import "./StyleHome.css";
 import "../index.css";
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/navbar";
+import { useContext } from "react";
+import { SignInContext } from "../context/signInContext";
 
 const Home = () => {
+  const { setIsLogged } = useContext(SignInContext); // Obtener el contexto de inicio de sesión
+
+  useEffect(() => {
+    localStorage.removeItem("userData");
+    setIsLogged(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div class="contenedor-inicial1">
-      <Navbar/>
+      <Navbar />
     </div>
   );
 };
