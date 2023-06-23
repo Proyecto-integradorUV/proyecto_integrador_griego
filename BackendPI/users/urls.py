f3324a82d7300104f63abd39ade573419c448400
Santiago:D <117051951+SantiagoCasanas@users.noreply.github.com>
2023-06-22 21:18:35 -0500

    Update urls users.py

--- BackendPI/users/urls.py
+++ BackendPI/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserList, LoginView, LogoutView, CreateUserView
+from .views import *
 
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path('list/', UserList.as_view(), name='user-list'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('update_user/<int:pk>/', UserUpdateAPIView.as_view(), name='update_user'),
 ]
+
