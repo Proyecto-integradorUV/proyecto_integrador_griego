91770358dde77a48692650bc958f3d9f7b3ed6ff
Santiago:D <117051951+SantiagoCasanas@users.noreply.github.com>
2023-06-22 21:34:39 -0500

    Update models prueba.py

--- BackendPI/prueba/models.py
+++ BackendPI/prueba/models.py
@@ -14,31 +14,13 @@ class Module(models.Model):
         return self.topic
 
 
-class Question(models.Model):
-    test = models.ForeignKey(Module, on_delete=models.CASCADE, null=False)
-    question_text = models.TextField(verbose_name='Texto de la pregunta.', null=False, blank=False)
-
-    def __str__(self):
-        return self.question_text
-
-
-
-class Option(models.Model):
-    question = models.ForeignKey(Question, related_name='Preguntas', on_delete=models.CASCADE, null=False)
-    is_correct = models.BooleanField(verbose_name='¿Es esta la respuesta?', default=False, null=False, blank=False)
-    answer = models.TextField(verbose_name='Texto de la respuesta.', null=False, blank=False)
-
-    def __str__(self):
-        return self.answer
-  
-
 
 class Test(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=False)
-    test = models.ForeignKey(Module, on_delete=models.CASCADE, null=False)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE, null=False)
     score = models.DecimalField(null = False, blank = False, max_digits=2, decimal_places=1)
     date = models.DateField(null=False, blank=False, default = datetime.date.today)
+    approved = models.BooleanField(default = False, null = False, blank = False)
 
     def __str__(self):
-        return self.score
-
+        return self.approved
